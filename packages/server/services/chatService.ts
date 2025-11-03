9127c88ee98b03ce3a147ca5eca4fcd8f96ff7da
Oreva-J <orevaawusi@gmail.com>
2025-11-03 21:04:43 +0100

    Prompt modified

--- packages/server/services/chatService.ts
+++ packages/server/services/chatService.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { conversationRepository } from "../db/chatRepository.js";
 import { llmClient } from "../llm/client.js";
-import template from '../prompts/chatbot.txt'
+import template from '../prompts/chatbot_instructions.txt'
 
 const aceInfo_db = fs.readFileSync(path.join(__dirname, "..", "prompts", "pharmacy_info.md"), "utf-8")
  const instructions = template.replace('{{aceInfo}}', aceInfo_db)
