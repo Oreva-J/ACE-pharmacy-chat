0b70c56b9a15562481b15dc3d74ef3da8e9b7604
Oreva-J <orevaawusi@gmail.com>
2025-11-03 20:50:09 +0100

    Pharmacy db info added and connected

--- packages/server/services/chatService.ts
+++ packages/server/services/chatService.ts
@@ -1,7 +1,12 @@
-import { conversationRepository } from "../db/chatRepository.js"
-import { llmClient } from "../llm/client.js"
 
-const instructions = "Hello"
+import fs from "fs";
+import path from "path";
+import { conversationRepository } from "../db/chatRepository.js";
+import { llmClient } from "../llm/client.js";
+import template from '../prompts/chatbot.txt'
+
+const aceInfo_db = fs.readFileSync(path.join(__dirname, "..", "prompts", "pharmacy_info.md"), "utf-8")
+ const instructions = template.replace('{{aceInfo}}', aceInfo_db)
 
 const sendMessage = async(prompt: string, conversationId: string)=>{
     const previousResponseId = conversationRepository.getLastResponseId(conversationId)
